4d78ab4473c7e86c7f5f088b1bccf6e678d74544
DikoeMati <40753078+DikoeMati@users.noreply.github.com>
2018-07-10 08:40:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys, numpy as np, logging
 import db
 logging.basicConfig(filename="log.log", level=logging.INFO)
+CTime = datetime.datetime.now().timestamp()
 try:
     f = open("data.txt", 'r')
 except IOError:
@@ -17,7 +18,7 @@ except ValueError:
 try:
     db.DataBase.callFunction('setUr', str(data), str(np.linalg.solve(result)))
 except mysql.connector.IntegrityError:
-    db.DataBase.callFunction('setLogs', str(np.linalg.solve(result)), str(curTime))
+    db.DataBase.callFunction('setLogs', str(np.linalg.solve(result)), str(CTime))
     logging.info("\nleveln: INFO message: equation: %s result: %s" % (data, result))
 f.close()
 
